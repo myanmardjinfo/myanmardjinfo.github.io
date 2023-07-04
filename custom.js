835979048e603b878e243d9c1bf613a6288709a0
myanmardjinfo <138562247+myanmardjinfo@users.noreply.github.com>
2023-07-04 16:22:54 +0630

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,6 @@
+function sayHi(){
+  var linkUrl = document.getElementById("linkUrl");
+  var url = linkUrl.value;
+  txtOutput.value = "https://myanmardjinfo.github.io/" + url +""
+  iframe.value = "https://myanmardjinfo.github.io/" + url +""
+  }
